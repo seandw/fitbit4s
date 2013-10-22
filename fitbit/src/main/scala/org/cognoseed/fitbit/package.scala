e7dbb30c0ca3e25227dd44fd5e6ecac5305f9d27
Sean Webster <sean@cognoseed.org>
2013-10-21 23:00:08 -0700

    Indentation should be consistent.

--- fitbit/src/main/scala/org/cognoseed/fitbit/package.scala
+++ fitbit/src/main/scala/org/cognoseed/fitbit/package.scala
@@ -5,15 +5,15 @@ import oauth.signpost.basic.DefaultOAuthProvider
 
 package object fitbit {
 
-    val provider = new DefaultOAuthProvider(
-      endpoints.RequestToken,
-      endpoints.AccessToken,
-      endpoints.Authorize
-    )
+  val provider = new DefaultOAuthProvider(
+    endpoints.RequestToken,
+    endpoints.AccessToken,
+    endpoints.Authorize
+  )
 
-    val consumer = new DefaultOAuthConsumer(
-      conf.consumerKey,
-      conf.consumerSecret
-    )
+  val consumer = new DefaultOAuthConsumer(
+    conf.consumerKey,
+    conf.consumerSecret
+  )
 
 }
