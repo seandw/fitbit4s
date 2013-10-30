4cda0717bc16982f370bd06798331bf20f8b370e
Sean Webster <sean@cognoseed.org>
2013-10-29 17:00:05 -0700

    Playing around with JSON.

--- fitbit/build.sbt
+++ fitbit/build.sbt
@@ -5,3 +5,5 @@ version := "0.0.1"
 scalaVersion := "2.10.3"
 
 libraryDependencies += "oauth.signpost" % "signpost-core" % "1.2.1.2"
+
+libraryDependencies += "org.json4s" %% "json4s-native" % "3.2.5"
--- fitbit/src/main/scala/me/seandw/FitbitApp.scala
+++ fitbit/src/main/scala/me/seandw/FitbitApp.scala
@@ -1,8 +1,19 @@
 package me.seandw
 import org.cognoseed.fitbit.FitbitClient
+import org.json4s._
+import org.json4s.native.JsonMethods._
 
 object FitbitApp extends App {
   val client = FitbitClient()
+  val res = client.getResource("activities/steps/date/today/1m.json")
+  val ast = parse(res)
 
-  println(client.getResource("activities/steps/date/today/1m.json"))
+  println(ast)
+  println(ast \\ "activities-steps")
+  for {
+    JArray(child) <- ast \\ "activities-steps"
+    JObject(entry) <- child
+    JField("dateTime", JString(dateTime)) <- entry
+    JField("value", JString(value)) <- entry
+  } println(dateTime + " -> " + value)
 }
