72450da757218898fbb52a6fecba10b51682b021
Sean Webster <sean@cognoseed.org>
2013-11-04 17:22:39 -0800

    The user is now responsible for getting/saving access tokens.

--- fitbit/src/main/scala/org/cognoseed/fitbit/FitbitClient.scala
+++ fitbit/src/main/scala/org/cognoseed/fitbit/FitbitClient.scala
@@ -9,15 +9,22 @@ import oauth.signpost.OAuthConsumer
 import oauth.signpost.basic.{DefaultOAuthConsumer, DefaultOAuthProvider}
 
 class FitbitClient(consumer: OAuthConsumer) extends FitbitEndpoints {
+  private lazy val provider =
+    new DefaultOAuthProvider(
+      RequestTokenUrl,
+      AccessTokenUrl,
+      AuthorizeUrl
+    )
 
   def this(
     consumerKey: String,
     consumerSecret: String,
-    accessToken: String,
-    accessTokenSecret: String
+    accessToken: String = null,
+    accessTokenSecret: String = null
   ) = {
     this(new DefaultOAuthConsumer(consumerKey, consumerSecret))
-    consumer.setTokenWithSecret(accessToken, accessTokenSecret)
+    if (accessToken != null && accessTokenSecret != null)
+      consumer.setTokenWithSecret(accessToken, accessTokenSecret)
   }
 
   def getResource(resource: String): String = {
@@ -29,7 +36,7 @@ class FitbitClient(consumer: OAuthConsumer) extends FitbitEndpoints {
     new BufferedSource(request.getInputStream()).mkString
   }
 
-  def store(stream: OutputStream) = {
+  def store(stream: OutputStream): Unit = {
     val prop = new Properties()
     prop.setProperty("consumerKey", consumer.getConsumerKey)
     prop.setProperty("consumerSecret", consumer.getConsumerSecret)
@@ -38,48 +45,7 @@ class FitbitClient(consumer: OAuthConsumer) extends FitbitEndpoints {
     prop.store(stream, "OAuth credentials for this application.")
   }
 
-  def getActivity(
-    activity: String,
-    range: String = "1m",
-    start: String = "today"
-  ) = {
-    getResource("activities/"+activity+"/date/"+start+"/"+range+".json")
-  }
-
-}
-
-object FitbitClient extends FitbitEndpoints {
-
-  def fromProperties(prop: Properties): FitbitClient = {
-    val (
-      consumerKey,
-      consumerSecret,
-      accessToken,
-      accessTokenSecret
-    ) = (
-      prop.getProperty("consumerKey"),
-      prop.getProperty("consumerSecret"),
-      prop.getProperty("accessToken"),
-      prop.getProperty("accessTokenSecret")
-    )
-    
-    val consumer = new DefaultOAuthConsumer(consumerKey, consumerSecret)
-
-    if (accessToken == null || accessTokenSecret == null)
-      getAccessTokens(consumer)
-    else
-      consumer.setTokenWithSecret(accessToken, accessTokenSecret)
-
-    new FitbitClient(consumer)
-  }
-
-  private def getAccessTokens(consumer: OAuthConsumer) = {
-    val provider = new DefaultOAuthProvider(
-      RequestTokenUrl,
-      AccessTokenUrl,
-      AuthorizeUrl
-    )
-
+  def getAccessTokens(): Unit = {
     val url = provider.retrieveRequestToken(consumer, null)
     println("Navigate to " + url + " to get your verifier.")
 
@@ -88,4 +54,24 @@ object FitbitClient extends FitbitEndpoints {
     provider.retrieveAccessToken(consumer, verifier)
   }
 
+
+  def getActivity(
+    activity: String,
+    range: String = "1m",
+    start: String = "today"
+  ): String = {
+    getResource("activities/"+activity+"/date/"+start+"/"+range+".json")
+  }
+
+}
+
+object FitbitClient {
+  def fromProperties(prop: Properties): FitbitClient = {
+    new FitbitClient(
+      prop.getProperty("consumerKey"),
+      prop.getProperty("consumerSecret"),
+      prop.getProperty("accessToken"),
+      prop.getProperty("accessTokenSecret")
+    )
+  }
 }
